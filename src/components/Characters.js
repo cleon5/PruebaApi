be9ab146744f638783389cc7372095fe8847d870
cleon5 <cleon5@ucol.mx>
2022-09-03 22:54:00 -0500

    update

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,26 +1,24 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Characters = ({ Characters = [] }) => {
-console.log(Characters)
+  console.log(Characters);
   return (
     <div className="row">
-      {Characters.map((item, index) => ( 
+      {Characters.map((item, index) => (
         <div key={index} className="col">
-          <Link to={`/Character/${item.id}`} >
-             <div className="card" style={{minWidth:"200px"}}>
-                <img src={item.image} alt=""/>
+          <Link to={`/Character/${item.id}`}>
+            <div className="card" style={{ minWidth: "200px" }}>
+              <img src={item.image} alt="" />
             </div>
           </Link>
-           
-            <div className="card-body">
-              
-                <h5 className="card-title">{item.name}</h5>
-                <hr/>
-                <p>Especie: {item.species}</p>
-                <p>Localizacion: {item.location.name}</p>
 
-            </div>
+          <div className="card-body">
+            <h5 className="card-title">{item.name}</h5>
+            <hr />
+            <p>Especie: {item.species}</p>
+            <p>Localizacion: {item.location.name}</p>
+          </div>
         </div>
       ))}
     </div>
